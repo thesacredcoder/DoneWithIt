97c9ffcf1a3084412b953fdb6e9d8e0c78557d9c
thesacredcoder <akashsamal4875@gmail.com>
2021-02-21 15:31:43 +0530

    Added the List Screen and some more re-usable components

--- app/screens/MesssagesScreen.js
+++ app/screens/MesssagesScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import ListItem from '../components/ListItem';
+
+import Constants from "expo-constants";
+import Screen from '../components/Screen';
+import ListItemSeparator from '../components/ListItemSeparator';
 
 const messages = [
     {
@@ -25,15 +29,23 @@ const messages = [
 
 function MesssagesScreen(props) {
     return (
-        <FlatList 
-            data={messages}
-            keyExtractor={message => message.id.toString()}
-            renderItem={({ item }) => 
-                <ListItem 
-                title={item.title}
-                subTitle={item.description}
-                image={item.image}/>} />
+        <Screen>
+            <FlatList 
+                data={messages}
+                keyExtractor={message => message.id.toString()}
+                renderItem={({ item }) => 
+                    <ListItem 
+                    title={item.title}
+                    subTitle={item.description}
+                    image={item.image}
+                    onPress={() => console.log("Message selected", item)}/>}
+                    ItemSeparatorComponent={ListItemSeparator} />
+                </Screen>
     );
 }
+
+// const styles = StyleSheet.create({
+   
+// })
 
 export default MesssagesScreen;
