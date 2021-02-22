83bd255bf45c97569b19a4d9d2f4653776ccaf31
thesacredcoder <akashsamal4875@gmail.com>
2021-02-22 11:18:28 +0530

    Added the Account and Listings Screens

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import {  View } from 'react-native';
-import AppButton from './app/components/AppButton';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MesssagesScreen from './app/screens/MesssagesScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import React from "react";
+import { View } from "react-native";
+import AppButton from "./app/components/AppButton";
+import AppTextInput from "./app/components/AppTextInput";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import Screen from "./app/components/Screen";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import MesssagesScreen from "./app/screens/MesssagesScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
     // <WelcomeScreen />
     // <ViewImageScreen />
     // <ListingDetailsScreen />
-    <MesssagesScreen />
+    // <MesssagesScreen />
+    // <AccountScreen />
+    // <ListingsScreen />
+    <Screen>
+      <AppTextInput placeholder="Write Something" icon="email" />
+    </Screen>
   );
 }
-
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,47 +1,49 @@
-import React from 'react';
-import { Image, View, StyleSheet } from 'react-native';
+import React from "react";
+import { Image, View, StyleSheet } from "react-native";
 
 import AppText from "../components/AppText";
 import ListItem from "../components/ListItem";
 import colors from "../config/colors";
 
 function ListingDetailsScreen(props) {
-    return (
-       <View>
-           <Image style={styles.image} source={require("../assets/jacket.jpg")} />
-           <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>Red Jacket for sale</AppText>
-            <AppText style={styles.price}>$100</AppText>
-            <View style={styles.userContainer}>
-                <ListItem
-                image={require("../assets/akash.jpg")}
-                title="Akash Samal"
-                subTitle="5 Listings" />
-            </View>
-           </View>
-       </View>
-    );
+  return (
+    <View>
+      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red Jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/akash.jpg")}
+            title="Akash Samal"
+            subTitle="5 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    detailsContainer: {
-        padding: 20,
-    },
-    image: {
-        width: "100%",
-        height: 300,
-    },
-    price: {
-        color: colors.secondary,
-        fontWeight: "bold",
-        fontSize: 20
-    },
-    title: {
-        fontSize: 24
-    },
-    userContainer: {
-        marginVertical: 15
-    },
-})
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    // overflow: "hidden",
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 24,
+  },
+  userContainer: {
+    marginVertical: 15,
+  },
+});
 
 export default ListingDetailsScreen;
